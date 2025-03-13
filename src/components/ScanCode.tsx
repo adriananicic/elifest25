e04997b85227ac726366d1a6d7523bae02b70143
Adrian <adriananicic@gmail.com>
2025-03-13 18:15:15 +0100

    debugging

--- src/components/ScanCode.tsx
+++ src/components/ScanCode.tsx
@@ -65,7 +65,7 @@ const ScanQRCodePage = () => {
       <div id="qr-scanner-container" style={{ width: "100%", height: "400px" }}>
         <Scanner
           onScan={(res) => {
-            alert("ALERTAN RES " + res);
+            alert("ALERTAN RES " + JSON.stringify(res));
             handleScan(res);
           }}
           onError={handleError}
